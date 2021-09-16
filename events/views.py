ff10cec4f1ab90f2c4a1fef48b2e6eea94951575
larkinz <andy_larkin33@hotmail.com>
2021-09-16 21:25:26 +0200

    feat: added events view and calendar items

--- events/views.py
+++ events/views.py
@@ -6,10 +6,22 @@ from .models import Event
 def all_events(request):
     """ A view to show all events """
 
-    events = Event.objects.all()
+    russia = Event.objects.get(event_id=2021001)
+    turkey = Event.objects.get(event_id=2021002)
+    us = Event.objects.get(event_id=2021003)
+    mexico = Event.objects.get(event_id=2021004)
+    brazil = Event.objects.get(event_id=2021005)
+    sa = Event.objects.get(event_id=2021006)
+    uae = Event.objects.get(event_id=2021007)
 
     context = {
-        'events': events,
+        'russia': russia,
+        'turkey': turkey,
+        'us': us,
+        'mexico': mexico,
+        'brazil': brazil,
+        'sa': sa,
+        'uae': uae,
     }
 
     return render(request, 'events/events.html', context)
