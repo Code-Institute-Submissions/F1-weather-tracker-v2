f534fb9af23a0caf8fa8a0dc39a4b9d83a580eed
larkinz <andy_larkin33@hotmail.com>
2021-09-19 23:06:43 +0200

    feat: added schedule model for weather app and set up basic template

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Schedule(models.Model):
+    event_id = models.IntegerField(primary_key=True)
+    practice1_start = models.CharField(max_length=16, blank=True, null=True)
+    practice2_start = models.CharField(max_length=16, blank=True, null=True)
+    practice3_start = models.CharField(max_length=16, blank=True, null=True)
+    qualifying_start = models.CharField(max_length=16, blank=True, null=True)
+    race_start = models.CharField(max_length=16, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Schedule'
--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from events.models import Event
+from .models import Schedule
 
 
 # Create your views here.
@@ -7,9 +8,11 @@ def daily_forecast(request, event_id):
     """ A view to show daily weather forecast data """
 
     event = get_object_or_404(Event, pk=event_id)
+    schedule = get_object_or_404(Schedule, pk=event_id)
 
     context = {
         'event': event,
+        'schedule': schedule,
     }
 
     return render(request, 'weather/daily_forecast.html', context)
