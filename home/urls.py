ebf103d1b60b9a111c1a79106a6ad69ffae6dfbc
larkinz <andy_larkin33@hotmail.com>
2021-09-21 20:17:29 +0200

    feat: added hamburger menu navigation

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from events.views import all_events
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path("events/", all_events, name="all_events"),
+    path('', views.index, name='home'),
 ]
