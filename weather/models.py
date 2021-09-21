e9c1994192ff2b8faaa4930e4a088257c1d1d020
larkinz <andy_larkin33@hotmail.com>
2021-09-21 05:31:40 +0200

    feat: add daily weather forecast model

--- weather/models.py
+++ weather/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Schedule(models.Model):
     event_id = models.IntegerField(primary_key=True)
@@ -12,3 +13,18 @@ class Schedule(models.Model):
     class Meta:
         managed = False
         db_table = 'Schedule'
+
+
+class Dailyweatherforecast(models.Model):
+    track_id_day = models.CharField(primary_key=True, max_length=5)
+    track = models.ForeignKey('events.Track', models.DO_NOTHING)
+    day = models.CharField(max_length=2)
+    date = models.CharField(max_length=16, blank=True, null=True)
+    min_temp = models.IntegerField(blank=True, null=True)
+    max_temp = models.IntegerField(blank=True, null=True)
+    precipitation = models.DecimalField(max_digits=65535, decimal_places=65535, blank=True, null=True)
+    chance_of_rain = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'DailyWeatherForecast'
