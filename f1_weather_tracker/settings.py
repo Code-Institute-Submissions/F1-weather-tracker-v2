741c507b0fbc6771dcb3e9af2037034aa274dd70
larkinz <andy_larkin33@hotmail.com>
2021-09-23 21:04:08 +0200

    maint: update allowed host

--- f1_weather_tracker/settings.py
+++ f1_weather_tracker/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'f1-weather-tracker-v2.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'f1-weather-v2.herokuapp.com']
 
 
 # Application definition
