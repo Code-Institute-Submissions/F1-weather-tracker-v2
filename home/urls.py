6a6c93aeed45148204964be7a7d82a767c0467be
larkinz <andy_larkin33@hotmail.com>
2021-09-24 08:56:40 +0200

    feat: set up basic profile page

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ from events.views import all_events
 
 urlpatterns = [
     path("events/", all_events, name="all_events"),
+    path("profile/", views.profile, name="profile"),
     path('', views.index, name='home'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 
 def index(request):
-    """ A view to return the index page """
+    """ A view to return the home page """
 
     return render(request, 'home/index.html')
+
+
+def profile(request):
+    """ A view to render the profile page """
+
+    if not request.user.is_authenticated:
+        return redirect('/accounts/signup/')
+
+    else:
+        return render(request, 'home/profile.html')
